88dbfa782aed8efa4bd7ccca8f4d58d48b82dac5
Himanshu <himanshu.mishra@vinsol.com>
2017-10-11 17:27:22 +0530

    done

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -3,26 +3,17 @@ function DropDownMenu(data) {
 }
 
 DropDownMenu.prototype.hoverIn = function(index) {
-  var _this = this;
-  return function() {
-    _this.nav.eq(index).children('ul').show();
-    _this.nav.eq(index).addClass('hover');
-  }
+  $(this).children('ul').show();
+  $(this).addClass('hover');
 }
 
 DropDownMenu.prototype.hoverOut = function(index) {
-  var _this = this;
-  return function() {
-    _this.nav.eq(index).children('ul').hide();
-    _this.nav.eq(index).removeClass('hover');
-  }
+  $(this).children('ul').hide();
+  $(this).removeClass('hover');
 }
 
 DropDownMenu.prototype.init = function() {
-  var _this = this;
-  this.nav.each(function(index) {
-    _this.nav.eq(index).hover(_this.hoverIn(index), _this.hoverOut(index));
-  })
+  this.nav.hover(this.hoverIn,this.hoverOut);
 }
 
 $(document).ready(function() {
