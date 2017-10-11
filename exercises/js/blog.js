18d0bb6e9eecc7fbb338df2c170d1e740a8ef383
Himanshu <himanshu.mishra@vinsol.com>
2017-10-11 18:37:31 +0530

    indentation

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -9,21 +9,21 @@ HeadlineEffect.prototype.eventHandler = function(blogLinkNumber) {
     event.preventDefault();
     _this.links.parent().siblings(_this.pClass).slideUp();
     _this.links.eq(blogLinkNumber).parent().siblings(_this.pClass).slideDown();
-  }
-}
+  };
+};
 
 HeadlineEffect.prototype.init = function() {
   var _this = this;
   this.links.each(function(index) {
     _this.links.eq(index).bind('click', _this.eventHandler(index));
-  })
-}
+  });
+};
 
 $(document).ready(function() {
   var data = {
     links: $('#blog a'),
     pClass : 'p.excerpt'
-  }
+  };
   var headlineEffectObject = new HeadlineEffect(data);
   headlineEffectObject.init();
-})
+});
