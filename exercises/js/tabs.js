477455180b8ef39b9e146178bdfbc6710b7c1e88
Himanshu <himanshu.mishra@vinsol.com>
2017-10-12 15:26:32 +0530

    done

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -4,53 +4,50 @@ function TabNagivation (data) {
 }
 
 TabNagivation.prototype.createUL = function() {
-  $('<ul id="tabs"></ul>').insertBefore(this.modules.eq(0));
-}
+  $('<ul id="tabs">').insertBefore(this.modules.eq(0));
+};
 
 TabNagivation.prototype.createNavigationBar = function() {
   var _this = this;
   this.modules.each(function(index) {
-    $('<li>' + _this.modules.eq(index).children('h2').text() + '</li>').appendTo('#tabs');
-  })
-  this.list = $('#tabs').children();
-}
+    $('<li>').text(_this.modules.eq(index).children('h2').text()).appendTo('#tabs');
+  });
+  this.list = $('#tabs').children('li');
+};
 
-TabNagivation.prototype.eventHandler = function(index) {
+TabNagivation.prototype.eventHandler = function() {
   var _this = this;
   return function() {
-    _this.modules.each(function(index_module) {
-      if(_this.modules.eq(index_module).children('h2').text() == _this.list.eq(index).text()) {
-        _this.modules.eq(index_module).show();
-        _this.modules.eq(index_module).addClass(_this.className);
+    var that = this;
+    _this.modules.each(function(index) {
+      if(_this.modules.eq(index).children('h2').text() == $(that).text()) {
+        _this.modules.eq(index).show();
+        _this.modules.eq(index).addClass(_this.className);
       } else {
-        _this.modules.eq(index_module).hide();
-        _this.modules.eq(index_module).removeClass(_this.className);
+        _this.modules.eq(index).hide();
+        _this.modules.eq(index).removeClass(_this.className);
       }
-    })
-  }
-}
+    });
+  };
+};
 
 TabNagivation.prototype.createNavigator = function() {
-  var _this = this;
-  this.list.each(function(index) {
-    _this.list.eq(index).bind('click',_this.eventHandler(index));
-  })
-}
+  this.list.click(this.eventHandler());
+};
 
 TabNagivation.prototype.init = function() {
   this.modules.hide();
   this.createUL();
   this.createNavigationBar();
   this.createNavigator();
-}
+};
 
 $(document).ready(function() {
-
   var data = {
     modules: $('.module'),
     className: 'current'
-  }
+  };
 
   var tabNavigationObject = new TabNagivation(data);
   tabNavigationObject.init();
-})
+});
