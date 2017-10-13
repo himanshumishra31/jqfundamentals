c4a4e7c617d377a63871c55828592c006d83234d
Himanshu <himanshu.mishra@vinsol.com>
2017-10-13 18:40:33 +0530

    exercise10

---
+++ exercises/js/specials.js
@@ -0,0 +1,50 @@
+function LoadContentJson(data) {
+  this.specialsDiv = data.specialsDiv;
+}
+
+LoadContentJson.prototype.init = function() {
+  $('<div>', { id:"checkBoxTarget" }).insertAfter(this.specialsDiv.find('form'));
+  this.specialsDiv.find('li.buttons').remove();
+  this.specialsDiv.find('form').find('select').change(this.eventHandler());
+};
+
+
+LoadContentJson.prototype.eventHandler = function() {
+  var _this = this;
+  return function() {
+    _this.getInfo($(this).val());
+  }
+}
+
+LoadContentJson.prototype.getInfo = function(selectedVal) {
+  var _this = this;
+  $.ajax({
+    url : "data/specials.json",
+    type : "GET",
+    dataType : 'json',
+  }).done(function(jsonResponse) {
+    _this.targetDivValue(jsonResponse[selectedVal]);
+  }).fail(function(xhr, status, errorThrown) {
+    alert("Error occured");
+  });
+};
+
+LoadContentJson.prototype.targetDivValue = function(jsonResponseInfo) {
+  this.targetDiv = $('#checkBoxTarget');
+  if(jsonResponseInfo) {
+    this.targetDiv.css('color', jsonResponseInfo.color);
+    this.targetDiv.attr('title', jsonResponseInfo.title);
+    this.targetDiv.text(jsonResponseInfo.text);
+    var img = jsonResponseInfo.image;
+    this.targetDiv.append($('<img>', {src : img.slice(1)}));
+  }
+};
+
+
+$(document).ready(function() {
+  var data = {
+    specialsDiv: $('#specials')
+  };
+  var loadContentJsonObject = new LoadContentJson(data);
+  loadContentJsonObject.init();
+});
